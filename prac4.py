2e1ed793274dcbea1c420f8a35d272e7a983aaaa
anryan <thoughtworks.shao@daimler.com>
2019-04-04 16:07:58 +0800

    prac4

---
+++ prac4.py
@@ -0,0 +1,18 @@
+
+'''
+Given an array of one's and zero's convert the equivalent binary value to an integer.
+
+Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
+Testing: [0, 0, 0, 1] ==> 1
+Testing: [0, 0, 1, 0] ==> 2
+Testing: [0, 1, 0, 1] ==> 5
+Testing: [1, 0, 0, 1] ==> 9
+Testing: [0, 0, 1, 0] ==> 2
+
+'''
+
+def binary_array_to_number(arr):
+  return int("".join(map(str, arr)), 2)
+
+
+print(binary_array_to_number([1, 1, 1, 1]))

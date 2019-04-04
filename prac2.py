38785801ceb79d3d26f9009b076f865efcf734aa
anryan <thoughtworks.shao@daimler.com>
2019-04-04 13:58:27 +0800

    prac2

--- prac2.py
+++ prac2.py
@@ -1,6 +1,7 @@
 
-"找出一串字符中最大和最小数字并输出"
+'''找出一串字符中最大和最小数字并输出
 
+way1
 def high_and_low(numbers):
   a = ' '.join(numbers.split())
   '先去掉空格，将string转化成数组'
@@ -10,12 +11,17 @@ def high_and_low(numbers):
   d = str(m[-1]) + " " + str(m[0])
   return d
 
+way2
+
+def high_and_low(numbers):
+    numbers.split(" ")
+    n=[int(x)for x in numbers.split(" ")]
+    print(max(n))
+
+'''
 
 
 
-  # ...
-  #return numbers
 numbers="4 5 29 54 4 0 -214 542 -64 1 -3 6 -6"
-
-Test.assert_equals(high_and_low("4 5 29 54 4 0 -214 542 -64 1 -3 6 -6"), "542 -214")
+high_and_low(numbers)
 

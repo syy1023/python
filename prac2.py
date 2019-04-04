f35d5b7c409611ee3f5ab9c12934d74ef8ffa244
anryan <thoughtworks.shao@daimler.com>
2019-04-04 11:48:50 +0800

    prac2

---
+++ prac2.py
@@ -0,0 +1,21 @@
+
+
+
+def high_and_low(numbers):
+  a = ' '.join(numbers.split())
+  '先去掉空格，将string转化成数组'
+  l = list(map(int, a.split()))
+  '排序'
+  m = sorted(l)
+  d = str(m[-1]) + " " + str(m[0])
+  return d
+
+
+
+
+  # ...
+  #return numbers
+numbers="4 5 29 54 4 0 -214 542 -64 1 -3 6 -6"
+
+Test.assert_equals(high_and_low("4 5 29 54 4 0 -214 542 -64 1 -3 6 -6"), "542 -214")
+
